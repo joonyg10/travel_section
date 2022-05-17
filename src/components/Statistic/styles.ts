12d73f9042611c13729dc8e5826212166a2cb9a3
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2022-05-17 17:30:50 +0900

    ADD: Statistic 컴포넌트에 대해 Counter부분를 표시할 AnimatedCount 컴포넌트 생성 및 min-width를 지정

--- src/components/Statistic/styles.ts
+++ src/components/Statistic/styles.ts
@@ -4,8 +4,19 @@ const Container = styled.p`
   font-size: 3rem;
 
   & + & {
-    margin-block: 1rem;
+    margin-block: 1.375rem;
   }
 `;
 
-export { Container };
+const AnimatedCount = styled.strong<{ textLength: number }>`
+  display: inline-block;
+  min-width: ${({ textLength }) => `${1.875 * textLength}rem`};
+  margin-right: -0.375rem;
+  font-weight: bold;
+`;
+
+const BoldText = styled.span`
+  font-weight: bold;
+`;
+
+export { Container, AnimatedCount, BoldText };
