c77778c443b0b54f11d7cf0e35f822f35398bdbf
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2022-05-17 17:33:59 +0900

    MODIFY: onAnimate를 useCallback으로 wrapping MODIFY: counter를 보여주는 부분을 AnimatedCount로 교체 MODIFY: user라는 propsf를 추가하여 사람, 리뷰등에 따라 명, 개수를 표시토록 변경

--- src/components/Statistic/index.tsx
+++ src/components/Statistic/index.tsx
@@ -1,26 +1,33 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 import useAnimation from "../../hooks/useAnimation";
 
-import { Container } from "./styles";
+import { Container, AnimatedCount, BoldText } from "./styles";
 
 interface StatisticProps {
   status: number;
+  user?: boolean;
   children: React.ReactNode;
 }
 
-function Statistic({ status, children }: StatisticProps) {
+function Statistic({ status, user, children }: StatisticProps) {
   const [counter, setCounter] = useState<number>(status);
 
-  const onAnimate = (progress: number) => {
-    setCounter(() => status * (1 - Math.pow(1 - progress, 3)));
-  };
+  const onAnimate = useCallback(
+    (progress: number) => {
+      setCounter(() => status * (1 - Math.pow(1 - progress, 3)));
+    },
+    [status]
+  );
   useAnimation({ onAnimate });
 
   return (
     <Container>
-      <span>{Math.round(counter)}</span>
-      {children}
+      <AnimatedCount textLength={`${status}`.length}>
+        {Math.round(counter)}
+      </AnimatedCount>
+      <BoldText>만 {user ? "명" : "개"}</BoldText>
+      <span>의 {children}</span>
     </Container>
   );
 }
