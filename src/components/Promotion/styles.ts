fe62765ab963a4cfaafd6e2bbdca41e3c7ddad97
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2022-05-17 17:38:58 +0900

    ADD: Fadein 애니메이션 추가 MODIFY: Container, Item에 대한 스타일 수정

--- src/components/Promotion/styles.ts
+++ src/components/Promotion/styles.ts
@@ -1,14 +1,42 @@
-import styled from "styled-components";
+import styled, { AlignProps, JustifyProps, keyframes } from "styled-components";
 
 const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
+  column-gap: 7em;
 
-  max-width: 1200px;
-  margin-inline: auto;
-  column-gap: 5em;
+  width: 1200px;
+  margin-inline: 9rem;
 `;
 
-const Item = styled.div``;
+const boxFadeIn = keyframes`
+  from {
+    transform: translateY(50%);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+interface ItemProps {
+  flex?: boolean;
+  justify?: JustifyProps;
+  align?: AlignProps;
+  flexRow?: boolean;
+  delayOrder?: number;
+}
+
+const Item = styled.div<ItemProps>`
+  position: relative;
+  ${({ theme, flex, justify, align = "stretch" }) =>
+    flex && theme.mixin.flex(justify, align)};
+  flex-direction: ${({ flexRow }) => (flexRow ? "column" : "row")};
+  justify-self: end;
+
+  opacity: 0;
+  animation: ${boxFadeIn} 700ms ease-in forwards;
+  animation-delay: ${({ delayOrder }) => `${(delayOrder ?? 0) * 100}ms`};
+`;
 
 export { Container, Item };
