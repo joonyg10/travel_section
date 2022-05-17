2efe51b174e5ea4a27555f02ed417a7af2160f5b
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2022-05-18 00:59:55 +0900

    MODIFY: 기존의 컴포넌트를 stack, typography, FadeIn 컴포넌트로 수정

--- src/components/Promotion/index.tsx
+++ src/components/Promotion/index.tsx
@@ -1,34 +1,37 @@
-import { Image, Statistic, Award } from "../";
+import { Image, Statistic, Award, FadeIn } from "../";
 
-import { Container, FadeInItem, Item } from "./styles";
+import { Container, Stack, Typography } from "./styles";
 
 function Promotion() {
   return (
     <Container>
-      <FadeInItem data-subText="2019년 2월 기준">
+      <FadeIn>
         <Image src="./assets/triple@2x.png" alt="main award" />
-      </FadeInItem>
-      <Item flex flexRow justify="space-around">
-        <FadeInItem delayOrder={1}>
+        <Typography>2019년 2월 기준</Typography>
+      </FadeIn>
+      <Stack direction="column" justify="space-around" align="flex-start">
+        <FadeIn delay={1}>
           <Statistic status={350} user>
             사용자
           </Statistic>
           <Statistic status={21}>리뷰</Statistic>
           <Statistic status={650}>저장</Statistic>
-        </FadeInItem>
-        <FadeInItem flex delayOrder={2}>
-          <Award
-            src="./assets/play-store@2x.png"
-            store="2018 구글 플레이스토어"
-            award="올해의 앱 최우수상 수상"
-          />
-          <Award
-            src="./assets/badge-apple@4x.png"
-            store="2018 애플 앱스토어"
-            award="오늘의 여행앱 선정"
-          />
-        </FadeInItem>
-      </Item>
+        </FadeIn>
+        <FadeIn delay={2}>
+          <Stack>
+            <Award
+              src="./assets/play-store@2x.png"
+              store="2018 구글 플레이스토어"
+              award="올해의 앱 최우수상 수상"
+            />
+            <Award
+              src="./assets/badge-apple@4x.png"
+              store="2018 애플 앱스토어"
+              award="오늘의 여행앱 선정"
+            />
+          </Stack>
+        </FadeIn>
+      </Stack>
     </Container>
   );
 }
