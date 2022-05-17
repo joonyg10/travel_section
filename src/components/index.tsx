6a24ba4f2bb736f7bc3a869c34f4ded21c56cacc
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2022-05-17 17:55:28 +0900

    DELETE: Stack 컴포넌트 삭제

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,6 @@
 import Image from "./Image";
-import Stack from "./Stack";
 import Statistic from "./Statistic";
 import Award from "./Award";
 import Promotion from "./Promotion";
 
-export { Image, Stack, Statistic, Award, Promotion };
+export { Image, Statistic, Award, Promotion };
