75e69b5720eed8686805a9a15af7b92848e6984d
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2022-05-17 14:25:43 +0900

    MODIFY: Image 컴포넌트에 사이즈 props 추가

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -3,10 +3,11 @@ import { ImageStyle } from "./styles";
 interface ImageProps {
   src: string;
   alt: string;
+  size?: number;
 }
 
-function Image({ src, alt }: ImageProps) {
-  return <ImageStyle src={src} alt={alt} />;
+function Image({ src, alt, size }: ImageProps) {
+  return <ImageStyle src={src} alt={alt} size={size} />;
 }
 
 export default Image;
--- src/components/Image/styles.ts
+++ src/components/Image/styles.ts
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
-const ImageStyle = styled.img`
+const ImageStyle = styled.img<{ size?: number }>`
   display: block;
   max-width: 100%;
+  width: ${({ size }) => `${size}rem`};
+  height: ${({ size }) => `${size}rem`};
+
   object-fit: cover;
 `;
 
