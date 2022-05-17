c3f2490cfa56b537f6174210bb98a59a1bdbcf73
joonyg10 <58040168+joonyg10@users.noreply.github.com>
2022-05-18 00:57:21 +0900

    MODIFY: Promotion의 Container 스타일 수정 ADD: Typography 컴포넌트 추가

--- src/components/Promotion/styles.ts
+++ src/components/Promotion/styles.ts
@@ -1,53 +1,22 @@
-import styled, { AlignProps, JustifyProps, keyframes } from "styled-components";
+import styled, { AlignProps, JustifyProps } from "styled-components";
 
 const Container = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  column-gap: 7em;
-
+  grid-template-columns: 1fr 1.5fr;
+  column-gap: 10em;
   width: 1200px;
-  margin-inline: 9rem;
-`;
-
-const boxFadeIn = keyframes`
-  from {
-    transform: translateY(50%);
-  }
-  to {
-    opacity: 1;
-    transform: translateY(0);
-  }
+  margin-inline: auto;
 `;
 
 interface ItemProps {
-  flex?: boolean;
   justify?: JustifyProps;
   align?: AlignProps;
-  flexRow?: boolean;
-  delayOrder?: number;
+  direction?: "row" | "column";
 }
 
-const Item = styled.div<ItemProps>`
-  position: relative;
-  ${({ theme, flex, justify, align = "stretch" }) =>
-    flex && theme.mixin.flex(justify, align)};
-  flex-direction: ${({ flexRow }) => (flexRow ? "column" : "row")};
-  justify-self: end;
-
-  &::after {
-    content: attr(data-subText);
-    position: absolute;
-    bottom: 15%;
-    inset-inline: 0;
-    text-align: center;
-    font-size: 1.25rem;
-  }
+const Typography = styled.p`
+  font-size: 1.25rem;
+  text-align: center;
 `;
 
-const FadeInItem = styled(Item)`
-  opacity: 0;
-  animation: ${boxFadeIn} 700ms ease-in forwards;
-  animation-delay: ${({ delayOrder }) => `${(delayOrder ?? 0) * 100}ms`};
-`;
-
-export { Container, Item, FadeInItem };
+export { Container, Typography };
